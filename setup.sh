e8db534aec20448b3b69ce4b0853e3ea602eb323
Hank Chou <htchou@protonmail.com>
2024-04-26 00:25:48 +0800

    modify dir name in setup script

--- setup.sh
+++ setup.sh
@@ -12,17 +12,17 @@ log() {
 
 # create project folder in parent folder
 cd ..
-rm -rf nycu-iass-dl2024-taiwanese-asr
-mkdir -p nycu-iass-dl2024-taiwanese-asr/
-log "created nycu-iass-dl2024-taiwanese-asr/ at parent directory."
+rm -rf nycu-iais-dl2024-taiwanese-asr
+mkdir -p nycu-iais-dl2024-taiwanese-asr/
+log "created nycu-iais-dl2024-taiwanese-asr/ at parent directory."
 log "start cloning espnet."
-cd nycu-iass-dl2024-taiwanese-asr
+cd nycu-iais-dl2024-taiwanese-asr
 git clone https://github.com/espnet/espnet.git
 cd espnet/tools
 log "installing conda environment..."
-# ./setup_anaconda.sh miniconda espnet 3.9
-# log "installing espnet..."
-# make
+./setup_anaconda.sh miniconda espnet 3.9
+log "installing espnet..."
+make
 log "espnet setup completed."
 
 # download and unzip data
@@ -31,12 +31,12 @@ cd ../../..
 rm -f nycu-iass-dl2024-taiwanese-asr.zip
 kaggle competitions download -c nycu-iass-dl2024-taiwanese-asr
 log "data unzip started."
-rm -rf nycu-iass-dl2024-taiwanese-asr-data
-unzip nycu-iass-dl2024-taiwanese-asr.zip -d nycu-iass-dl2024-taiwanese-asr-data/ > /dev/null
+rm -rf nycu-iais-dl2024-taiwanese-asr-data
+unzip nycu-iass-dl2024-taiwanese-asr.zip -d nycu-iais-dl2024-taiwanese-asr-data/ > /dev/null
 log "data unzip completed."
 
 # create my receipe folder
-cd nycu-iass-dl2024-taiwanese-asr
+cd nycu-iais-dl2024-taiwanese-asr
 find espnet/egs2/ -mindepth 1 -maxdepth 1 ! -name 'TEMPLATE' -exec rm -rf {} \;
 cd espnet
 ./egs2/TEMPLATE/asr1/setup.sh egs2/my-receipe/asr1
@@ -44,30 +44,30 @@ log "my-receipe created in espnet/egs2."
 
 # move data into receipe folder
 cd ../..
-mkdir -p nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/downloads/
-mv nycu-iass-dl2024-taiwanese-asr-data/* nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/downloads/
-rm -rf nycu-iass-dl2024-taiwanese-asr-data
+mkdir -p nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/downloads/
+mv nycu-iais-dl2024-taiwanese-asr-data/* nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/downloads/
+rm -rf nycu-iais-dl2024-taiwanese-asr-data
 log "moved train/, test/ to under my-receipe/asr1/downloads/."
 
 # copy scripts and configs into receipe folder
-mkdir -p nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/local
-cp report1/scripts/data.sh nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/local/data.sh
-cp report1/scripts/data_prep.py nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/local/data_prep.py
-mkdir -p nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/conf
-cp report1/conf/* nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/conf
-cp report1/scripts/run_task1.sh nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/run_task1.sh
-cp report1/scripts/run_task2.sh nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/run_task2.sh
-cp report1/scripts/run_task3.sh nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/run_task3.sh
-cp report1/scripts/cleanup.sh nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/cleanup.sh
-cp report1/scripts/export_test_to_submission.py nycu-iass-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/export_test_to_submission.py
+mkdir -p nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/local
+cp report1/scripts/data.sh nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/local/data.sh
+cp report1/scripts/data_prep.py nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/local/data_prep.py
+mkdir -p nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/conf
+cp report1/conf/* nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/conf
+cp report1/scripts/run_task1.sh nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/run_task1.sh
+cp report1/scripts/run_task2.sh nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/run_task2.sh
+cp report1/scripts/run_task3.sh nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/run_task3.sh
+cp report1/scripts/cleanup.sh nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/cleanup.sh
+cp report1/scripts/export_test_to_submission.py nycu-iais-dl2024-taiwanese-asr/espnet/egs2/my-receipe/asr1/export_test_to_submission.py
 log "scripts and config files copied into my-receipe."
 
 # install additional dependencies
-cd nycu-iass-dl2024-taiwanese-asr/espnet/tools
+cd nycu-iais-dl2024-taiwanese-asr/espnet/tools
 log "installation of additional dependencies started."
 bash -c ". activate_python.sh; ./installers/install_s3prl.sh"
 bash -c ". activate_python.sh; ./installers/install_whisper.sh"
 bash -c ". activate_python.sh; pip install loralib"
 log "installed s3prl, whisper, loralib."
-log "project folder nycu-iass-dl2024-taiwanese-asr is ready."
+log "project folder nycu-iais-dl2024-taiwanese-asr is ready."
 
